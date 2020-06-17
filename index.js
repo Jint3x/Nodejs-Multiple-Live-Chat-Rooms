4e92e35740196c7c968f01d2c1f99e90cb4aff87
unknown <jintexgaming@gmail.com>
2020-06-17 16:28:41 +0300

    First Part of the Live Chat
    * Managed to create multiple chat rooms and connect them by adding their
    values to an array and then searching through that array once someone
    accesses a page with a chat route (Chat routes are the same as websocket
    routes)

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const path = require("path")
+const app = express();
+const http = require("http")
+const port = 3000;
+const WebSocket = require('ws');
+
+
+//const wss1 = new WebSocket.Server({ noServer: true });
+const server = http.createServer();
+
+
+let sockets = []
+
+// Create a webhook when someone accesses it by using a wildcard id and making a websocket based on it, then pushing it to an arr "sockets"
+app.get("/chat/:id", (req, res) => {
+    let id = req.params.id
+
+
+    let newSocket = id = new WebSocket.Server({ noServer: true})
+    sockets.push([newSocket, req.params.id])
+
+    newSocket.on('connection', function connection(ws) {
+        console.log("connected")
+        ws.send("test")
+
+        ws.on("message", (message) => {
+            console.log(message)
+        })
+      });
+
+    res.sendFile(path.join(__dirname + `/pages/${req.params.id}.html`))
+
+})
+
+
+
+
+app.listen(port, () => console.log("Server online"))
+
+
+// Whenever someone goes to a websocket ruote, check their url against an arr with all available websockets,
+// when if it can be found connect them to that socket
+server.on('upgrade', function upgrade(request, socket, head) {
+    const pathname = request.url.replace("/", "");
+    let socketPlace = sockets.findIndex(element => element[1] === pathname)
+ 
+    if (socketPlace + 1) {
+    sockets[socketPlace][0].handleUpgrade(request, socket, head, function done(ws) {
+    sockets[socketPlace][0].emit('connection', ws, request);
+      });
+    }
+     else {
+      socket.destroy();
+    }
+  });
+
+
+server.listen(8080);
+
+
+
